a8a91da4359fbd3ce3c6bf73bdd0709da616ef29
Naoya Yamashita <conao3@gmail.com>
2024-03-31 02:09:08 +0900

    read with timeout

--- src/kilo/core.clj
+++ src/kilo/core.clj
@@ -1,6 +1,18 @@
 (ns kilo.core
   (:gen-class))
 
+(defn byte-seq [^java.io.BufferedReader reader]
+  (lazy-seq
+   (if (not (.ready reader))
+     (do
+       (Thread/sleep 100)
+       (cons 0 (byte-seq reader)))
+     (let [ch (.read reader)]
+       (if (= ch -1)
+         '()
+         (cons ch (byte-seq reader)))))))
+
 (defn -main [& args]
   (println "hello, world")
-  (println (read-line)))
+  (doseq [elm (byte-seq (new java.io.BufferedReader *in*))]
+    (println elm)))
