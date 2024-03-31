4761d60b1cb4352df9a1fc28c7643d3c6b4ff1d5
Naoya Yamashita <conao3@gmail.com>
2024-03-31 13:32:53 +0900

    add stty funcitons

--- src/kilo/core.clj
+++ src/kilo/core.clj
@@ -1,5 +1,35 @@
 (ns kilo.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.string :as string]
+   [clojure.java.shell :as shell]))
+
+(defn stty-exec [& args]
+  (println (string/join " " args))
+  (apply shell/sh "/usr/bin/env" "stty" args))
+
+(defn stty-get-current-settings []
+  (stty-exec "-g"))
+
+(defn stty-set-current-settings [settings]
+  (stty-exec settings))
+
+(defn stty-change-key-echo [enabledp]
+  (stty-exec (if enabledp "echo" "-echo")))
+
+(defn stty-change-canonical-mode [enabledp]
+  (stty-exec (if enabledp "icanon" "-icanon"))
+  (when-not enabledp
+    (stty-exec "min" "1")))
+
+(defn stty-change-key-stroke-signals [enabledp]
+  (stty-exec "intr" (if enabledp "^C" "undef")))
+
+(defn exec! [handler]
+  (let [res (handler)]
+    (if (not (= (res :exit) 0))
+      (throw (new RuntimeException (string/trim (res :err))))
+      (string/trim (res :out)))))
 
 (defn byte-seq [^java.io.BufferedReader reader]
   (lazy-seq
@@ -13,7 +43,16 @@
          (cons ch (byte-seq reader)))))))
 
 (defn -main [& args]
-  (println "hello, world")
-  (doseq [elm (byte-seq (new java.io.BufferedReader *in*))]
-    (when (= elm (int \q)) (System/exit 1))
-    (println elm)))
+  (try
+    (let [res (exec! stty-get-current-settings)]
+      (try
+        (println "hello, world")
+        (exec! (fn [] stty-change-key-echo false))
+        (doseq [elm (byte-seq (new java.io.BufferedReader *in*))]
+          (when (= elm (int \q)) (System/exit 1))
+          (println elm))
+        (finally
+          (exec! (fn [] (stty-set-current-settings res))))))
+    (catch Exception e
+      (println (.getMessage e))
+      (System/exit 1))))
