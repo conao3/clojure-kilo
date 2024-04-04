10eefd9ac7085a714072d5c59630fc5b35c88d87
Naoya Yamashita <conao3@gmail.com>
2024-04-05 07:33:58 +0900

    implement read1

--- src/kilo/core.clj
+++ src/kilo/core.clj
@@ -8,29 +8,6 @@
   (println (string/join " " args))
   (apply shell/sh "/usr/bin/env" "stty" args))
 
-(defn stty-get-current-settings []
-  (stty-exec "-g"))
-
-(defn stty-set-current-settings [settings]
-  (stty-exec settings))
-
-(defn stty-change-key-echo [enabledp]
-  (stty-exec (if enabledp "echo" "-echo")))
-
-(defn stty-change-canonical-mode [enabledp]
-  (stty-exec (if enabledp "icanon" "-icanon"))
-  (when-not enabledp
-    (stty-exec "min" "1")))
-
-(defn stty-change-key-stroke-signals [enabledp]
-  (stty-exec "intr" (if enabledp "^C" "undef")))
-
-(defn exec! [handler]
-  (let [res (handler)]
-    (if (not (= (res :exit) 0))
-      (throw (new RuntimeException (string/trim (res :err))))
-      (string/trim (res :out)))))
-
 (defn byte-seq [^java.io.BufferedReader reader]
   (lazy-seq
    (if (not (.ready reader))
@@ -42,17 +19,29 @@
          '()
          (cons ch (byte-seq reader)))))))
 
+(defn read1 [^java.io.BufferedReader reader]
+  (loop [cnt 0]
+    (when (and (not (.ready reader)) (< cnt 10))
+      (println "waiting" cnt)
+      (Thread/sleep 200)
+      (recur (inc cnt))))
+  (if (.ready reader)
+    (.read reader)
+    0))
+
 (defn -main [& args]
-  (try
-    (let [res (exec! stty-get-current-settings)]
-      (try
-        (println "hello, world")
-        (exec! (fn [] stty-change-key-echo false))
-        (doseq [elm (byte-seq (new java.io.BufferedReader *in*))]
-          (when (= elm (int \q)) (System/exit 1))
-          (println elm))
-        (finally
-          (exec! (fn [] (stty-set-current-settings res))))))
-    (catch Exception e
-      (println (.getMessage e))
-      (System/exit 1))))
+  ;; (try
+  ;;   (let [res (exec! stty-get-current-settings)]
+  ;;     (try
+  ;;       (println "hello, world")
+  ;;       (exec! (fn [] stty-change-key-echo false))
+  ;;       (doseq [elm (byte-seq (new java.io.BufferedReader *in*))]
+  ;;         (when (= elm (int \q)) (System/exit 1))
+  ;;         (println elm))
+  ;;       (finally
+  ;;         (exec! (fn [] (stty-set-current-settings res))))))
+  ;;   (catch Exception e
+  ;;     (println (.getMessage e))
+  ;;     (System/exit 1)))
+  (let [inpt (read1 (java.io.BufferedReader. *in*))]
+    (println inpt)))
