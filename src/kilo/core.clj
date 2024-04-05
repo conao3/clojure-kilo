3df2c1e6f6cafbe2b5bb2b8828fb3fb9fc967b90
Naoya Yamashita <conao3@gmail.com>
2024-04-05 12:49:44 +0900

    enter raw mode

--- src/kilo/core.clj
+++ src/kilo/core.clj
@@ -8,7 +8,8 @@
 ;;; utils
 
 (defn exec [& args]
-  (println "exec" args)
+  (print "exec" args "\r\n")
+  (flush)
   (let [pb (java.lang.ProcessBuilder. (into-array String args))]
     (.redirectInput pb java.lang.ProcessBuilder$Redirect/INHERIT)
 
@@ -20,7 +21,8 @@
 (defn read1 [^java.io.BufferedReader reader]
   (loop [cnt 0]
     (when (and (not (.ready reader)) (< cnt 10))
-      (println "waiting" cnt)
+      (print "waiting: " cnt "\r\n")
+      (flush)
       (Thread/sleep 200)
       (recur (inc cnt))))
   (if (.ready reader)
@@ -31,8 +33,14 @@
 ;;; terminal
 
 (defn enable-raw-mode []
-  (useful.utils/returning (exec "/usr/bin/env" "stty" "-g")
-    (exec "/usr/bin/env" "stty" "-echo" "-icanon" "min" "1")))
+  (useful.utils/returning
+   (exec "/usr/bin/env" "stty" "-g")
+   (exec "/usr/bin/env" "stty" "-brkint" "-icrnl" "-inpck" "-istrip" "-ixon")
+   (exec "/usr/bin/env" "stty" "-opost")
+   (exec "/usr/bin/env" "stty" "cs8")
+   (exec "/usr/bin/env" "stty" "-echo" "-icanon" "-iexten" "-isig")
+   (exec "/usr/bin/env" "stty" "min" "0")
+   (exec "/usr/bin/env" "stty" "time" "1")))
 
 (defn disable-raw-mode [terminal-config]
   (exec "/usr/bin/env" "stty" terminal-config))
@@ -42,7 +50,9 @@
 
 (defn -main [& args]
   (let [terminal-config (enable-raw-mode)]
-    (println "terminal-config: " terminal-config)
+    (print "terminal-config: " terminal-config "\r\n")
+    (flush)
     (let [inpt (read1 (java.io.BufferedReader. *in*))]
-      (println inpt))
+      (print inpt "\r\n")
+      (flush))
     (disable-raw-mode terminal-config)))
